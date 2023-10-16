5102abfa25aece4a8152cb717a54585ff5cd93ce
Mattxt03 <matteopgschool7@gmail.com>
2023-10-16 11:36:52 +0200

    develop evenOrOdd

--- evenOrOdd.js
+++ evenOrOdd.js
@@ -1,17 +1,17 @@
 const evenOrOdd = (arr) => {
     let e = 0;
     let o = 0;
-    for (let x = 0; x < arr.length; x++) {
-        
-        if (arr[x] % 2 === 0 ) {
+
+    arr.forEach((element) => {
+        if (element % 2 === 0 ) {
             e += 1;
         }
         else o += 1;
-    }
+    });
 
     return "Even: " + e + ", Odds: " + o;
 }
 
-const myArr = [1, 4, 2, 7, 6, 9];
+const myArr = [1, 4, 2, 7, 6, 9, 3, 3, 8];
 
 console.log(evenOrOdd(myArr));
