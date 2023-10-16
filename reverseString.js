cd5a371c664144629ba62b405a3b266556daa7c2
LucaMichele92 <kelediluca@gmail.com>
2023-10-16 11:04:31 +0200

    reversString

---
+++ reverseString.js
@@ -0,0 +1,7 @@
+
+function reverseString(inputString) {
+    return inputString.split('').reverse().join('');
+}
+
+var reversedString = reverseString("hello - or hello mum!");
+console.log(reversedString); 
